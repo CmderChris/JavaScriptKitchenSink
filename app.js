1d586e1425b1cd792a09365220a1e9eea99704c0
Chris Smith <christopherkeithsmith@gmail.com>
2019-09-30 18:52:12 -0500

    probably good

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myName = 'Chris';
+let myName = 'Chris';
 const numOfStates = 50;
 let add = 5 + 4;
 
@@ -24,3 +24,26 @@ let favoriteVegetables = ['Onions', 'Carrots', 'Squash', 'Tomatoes'];
 for(let i = 0; i < favoriteVegetables.length; i++) {
     console.log(favoriteVegetables [i]);
 }
+
+let family = [['Emily', 24], ['Keith', 60], ['Carol', 59], ['Oliver', 1], ['Julian', 3]];
+
+for (let i = 0; i < family.length; i++) {
+    checkAge(family[i][0], family[i][1]);
+  };
+
+function getLength(word) {
+    console.log(word.length);
+  };
+
+let helloWorld = ('Hello World');
+getLength(helloWorld);
+
+function oddOrEven(wordLength) {
+    if (wordLength.length % 2 === 0) {
+      console.log('The world is nice and even!');
+    } else {
+      console.log('The world is an odd place!');
+    };
+  };
+  
+  oddOrEven(helloWorld);
