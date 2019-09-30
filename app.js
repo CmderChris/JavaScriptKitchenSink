f718ea312c2224c135582a0d83950e91410e31fe
Chris Smith <christopherkeithsmith@gmail.com>
2019-09-30 18:16:05 -0500

    added array loop

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ function sayHello() {
 };
 
 sayHello();
+
+function checkAge(name, age) {
+    if (age < 21) {
+      alert(`Sorry ${name}, you aren't old enough to view this page!`);
+    };
+  };
+
+checkAge ('Charles', 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("Mark", 17);
+
+let favoriteVegetables = ['Onions', 'Carrots', 'Squash', 'Tomatoes'];
+
+for(let i = 0; i < favoriteVegetables.length; i++) {
+    console.log(favoriteVegetables [i]);
+}
