fde3ef26d01cd78706f6ae179869db98a9444630
Chris Smith <christopherkeithsmith@gmail.com>
2019-10-01 10:07:04 -0500

    last commit

--- app.js
+++ app.js
@@ -3,47 +3,73 @@ const numOfStates = 50;
 let add = 5 + 4;
 
 function sayHello() {
-    console.log('Hello World!')
+  console.log('Hello World!')
 };
 
 sayHello();
 
+// Write a function that checks age
 function checkAge(name, age) {
-    if (age < 21) {
-      alert(`Sorry ${name}, you aren't old enough to view this page!`);
-    };
+  if (age < 21) {
+    alert('Sorry ' + name + ',you aren\'t old enough to view this page!');
   };
+};
 
-checkAge ('Charles', 21);
+checkAge('Charles', 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("Mark", 17);
 
-let favoriteVegetables = ['Onions', 'Carrots', 'Squash', 'Tomatoes'];
+let favoriteVegetables = ['Onions', 'Carrots', 'Squash', 'Tomatoes', 'Dumbass'];
 
-for(let i = 0; i < favoriteVegetables.length; i++) {
-    console.log(favoriteVegetables [i]);
+for (let i = 0; i < favoriteVegetables.length; i++) {
+  console.log(favoriteVegetables[i]);
 }
 
-let family = [['Emily', 24], ['Keith', 60], ['Carol', 59], ['Oliver', 1], ['Julian', 3]];
-
+// let family = [['Emily', 24], ['Keith', 60], ['Carol', 59], ['Oliver', 1], ['Julian', 3]];
+const family = [
+  {
+    name: 'Emily',
+    age: 24
+  },
+  {
+    name: 'Keith',
+    age: 60
+  },
+  {
+    name: 'Carol',
+    age: 59
+  },
+  {
+    name: 'Oliver',
+    age: 1
+  },
+  {
+    name: 'Julian',
+    age: 3
+  },
+  {
+    name: 'MY FRIEND LUKE LMAO',
+    age: 999
+  },
+];
 for (let i = 0; i < family.length; i++) {
-    checkAge(family[i][0], family[i][1]);
-  };
+  checkAge(family[i].name, family[i].age);
+};
 
 function getLength(word) {
-    console.log(word.length);
-  };
+  return word.length
+};
 
-let helloWorld = ('Hello World');
-getLength(helloWorld);
 
-function oddOrEven(wordLength) {
-    if (wordLength.length % 2 === 0) {
-      console.log('The world is nice and even!');
-    } else {
-      console.log('The world is an odd place!');
-    };
-  };
+// let helloLength = getLength('Hello World!');
   
-  oddOrEven(helloWorld);
+function oddOrEven(wordLength) {
+  if (wordLength % 2 === 0) {
+    console.log('The world is nice and even!');
+  } else {
+    console.log('The world is an odd place!');
+  };
+};
+
+oddOrEven(getLength('Hello World'));
